e0c110b4667e6fc2c3275f1559daf06844faccf2
Raheem Khaled <raheemkhaled20@gmail.com>
2021-04-18 08:16:08 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,72 +1,119 @@
-  /* Global Variables */
-   
+
+/* Global Variables */
+//define the link of Url
+const link = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+//define API key
+const key = 'b18e6b87a3efa2f901357c94da17d4ea';
+//define path of the url
+const addkey = '&units=metric&APPID=';  
+//define the div of feeling in a variable
+const feelings      = document.querySelector('#feelings');
+//define the button
+const Generate   = document.querySelector('#generate');
+//define the div of datails which posts
+const div= document.querySelector("#entryHolder");
+//define the zip code
+const zipcode = document.querySelector('#zip');
+  
+
+
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+1 +'.'+ d.getDate()+'.'+ d.getFullYear();
-//define the api code
-const KeyOfApi = "b18e6b87a3efa2f901357c94da17d4ea";
-//define the bottun of generate to add an event
-const Generate = document.querySelector("#generate");
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+//define the object data, in which we received the data
+const data = {};
 
-Generate.addEventListener('click' , async () => {
-    //getting the value of the zip code and what user is feeling
-    const zipcode = document.querySelector("#zip").value ;
-    const feel = document.querySelector("#feelings").value;
-    // 2 if statement to make user write his feeling and the zip code
-    if(!zipcode){
-        return alert ("Please, enter the Zip Code")
+//getDate function by which know the date
+const getDate = () => {
+    const date = new Date();
+    //transform the date to string
+    return date.toDateString();
+  }
+
+//define the action function by which the event perform
+const action = async function (e){
+    //pick up the value of zip code which is typed in a variable
+    const newZip =  zipcode.value;
+    //call the weather function to get the data about weather by the zip code
+    weather(link,newZip,addkey,key) ;
+    //call the savedata function to save the data which is recieved
+    savedata();
     }
-    if(!feel){
-        return alert ("Please, enter What are you feeling")
-    }
-    
-    GettingData (zipcode , feel)
-    .then(function(data){
-        console.log(data);
-        
-        postData('/add', {date:d , temp:data , content:feel})
-        updateUI();
-    })
-    
-})
 
-//function by which we get the data
-async function GettingData(zipcode , feel){
-    //url which getting from it the response 
-    const Res = await fetch (`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${KeyOfApi}&units=metric`)
-        if ( Res.status === 404 || Res.status === 400 ){
-            return alert("ENTER VALID ZIP CODE");
+//add an event to the button of generate calling the action function    
+Generate.addEventListener('click' , action); 
+
+
+   
+//asynnc function by which we get the data about weather, our parameters are the all of url which consists of the link + newzip + addkey + key
+async function weather (link , newZip , addkey , key){
+    //define the response that = the excuting of this link after that try to collect the data as a json and return it, and if it is an error console it to make me can handle with it 
+    const res = await fetch (link+newZip+addkey+key)
+      
+        try {
+            const data = await res.json();
+            console.log(data)
+            return data;
+          }  catch(error) {
+            console.log("error", error);
+          }
         }
-    try{
-        //getting the temp from the data which is recieved
-        const weather = await Res.json();
-        const temp = weather.main.temp;        
-        console.log(temp);
-        return temp;
 
-    }catch(err){
-        //to handle any error
-        console.log("err",err)
+
+          
+//async function to save the data and collect it in its vaiable in the data object
+const savedata = async () => {
+    //call the getDate function to pick the date and update the object
+    data.date = getDate();
+    //update the feelings by equal it to the value which user typed
+    data.feelings = feelings.value;
+    //call the Temp function after is excuted to update the temp in the function
+    data.temp = await Temp();
+    //calling the update function
+    updateUI();
     }
-}
+
+//Temp function to pick up the temperature
+const Temp = async()=> {
+    const zip = zipcode.value;
+
+    const endpoint = link + zip + addkey + key;
+    try {
+        const response = await fetch(endpoint);
+        if(response.ok) {
+          const jsonResponse = await response.json();
+          return jsonResponse.main.temp;
+        }
+      } catch(error) {
+        console.log(error.message);
+      }
+    }
+
+
+
+
+
 
 //function by which we post the data
-const postData = async ( url = "" , data = {})=>{
+const postData = async ( url = "/add" , data = {})=>{
     console.log(data);
     const response = await fetch (url , {
         method: 'POST',
         credentials: 'same-origin',
         headers: {
             'Content-Type' : 'application/json',
+            'Accept': 'application/json'
+
         },
         body: JSON.stringify(data)
     });
+    console.log(data);
     try{
 
-        const newDate = await response.json();
-        console.log(newDate);
-        return newDate;
+        const newData = await response.json()
+        return newData;
     }catch(error){
         console.log("error" , error);
     }
@@ -76,15 +123,12 @@ const updateUI = async ()=>{
     const request = await fetch('/all');
     try{
         const allData= await request.json();
-    
-      document.getElementById('date').innerHTML = `Date: ${allData.date} `;
-      document.getElementById('temp').innerHTML = `Temperature: ${allData.temp} `;
-      document.getElementById('content').innerHTML = `Content: ${allData.content} `;    
+        date.innerHTML = 'Today :' + data.date;
+        temp.innerHTML = 'Temperature is ' + data.temp + '&deg;C';
+        content.innerHTML = 'Feelings: ' + data.feelings;
+
+       
     }catch(error){
         console.log("error" , error);
     }
 }
-
-
-
-
